1ffb692d537a828557d695dc9baad83fc44b1bf6
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2019-11-10 11:19:34 +0530

    asset

---
+++ myfile.py
@@ -0,0 +1,35 @@
+from flask import Flask,request,render_template
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='root'
+app.config['MYSQL_DB']='abc'
+app.config['MYSQL_CURSORCLASS']='DictCursor'
+mysql=MySQL(app)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    # if request.method=='POST':
+    #     a=request.form['x']
+    #     b=request.form['y']
+    #     return render_template('hello.html',sum=int(a)+int(b))
+    cur=mysql.connection.cursor()
+    #cur.execute('''show tables''')
+    cur.execute("SELECT * FROM products_product")
+    results=cur.fetchall()
+    print(results)
+    if request.method=='POST':
+        id=request.form['id']
+        title=request.form['title']
+        desc=request.form['desc']
+        p_id=request.form['p_id']
+        cur.execute("""INSERT INTO products_product
+                 values(%s,%s,%s,%s)""",(id,title,desc,p_id))
+        mysql.connection.commit()
+        #cur.execute('''INSERT INTO products_product
+         #           values(13,'asdasda','fasgasgas',525)''')
+    return render_template('tables.html',data=results)
+
+if __name__=="__main__":
+    app.run()
