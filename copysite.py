d7aa3c16799abaca30461e7685884ce1ab39a9e1
Jaspreet8843 <Jaspreet8843@users.noreply.github.com>
2019-12-02 18:41:13 +0530

    Revert "ui"
    This reverts commit 80ce20516c1ccc5730dfe8f58474a6021fb0bc17.

--- copysite.py
+++ copysite.py
@@ -847,11 +847,8 @@ def emp_logout():
 @app.route('/emp',methods=['GET','POST'])
 def emp():
     if 'EmpAccess' in session:
-        #print(session['designation'])
-        cur=mysql.connection.cursor()
-        cur.execute("""SELECT fname FROM employee WHERE employee_id=%s""",(session['id'],))
-        name=cur.fetchone()
-        return render_template('/employee/employee.html',name=name['fname'],desg=session['designation'],tab="stats")
+        print(session['designation'])
+        return render_template('/employee/employee.html',desg=session['designation'],tab="stats")
     return redirect(url_for('emp_access'))
 
 #Employee Profile Method.
